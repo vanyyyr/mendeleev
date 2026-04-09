b2ad74ed1344aed505a67e17a46b57ebab7ed8c1
vanyyyr <ratnikovivan070@gmail.com>
2026-04-09 17:36:08 +0300

    bjb

--- components/PeriodicTable.tsx
+++ components/PeriodicTable.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { elements, ChemicalElement } from '../lib/elements';
 import styles from './PeriodicTable.module.css';
 import StoryModal from './StoryModal';
